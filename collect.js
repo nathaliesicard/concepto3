83aa070df852b9d4df8d346b0d70d58381d7a4dd
nathaliesicard <nathalie.sicard@gmail.com>
2016-09-25 18:12:43 -0500

    Some work in progress

--- collect.js
+++ collect.js
@@ -2,7 +2,7 @@
  * Created by nathaliesicard on 9/24/16.
  */
 var fs = require('fs');
-const arr = require('fs').readdirSync('./public/img/corporativos');
+const arr = require('fs').readdirSync('./public/img/culturales');
 for (let i = 0; i < arr.length; i++) {
-  console.log('<img src="/img/corporativos/'+arr[i]+'" alt="Evento Corporativo Concepto3" data-caption="Evento">');
+  console.log('<img src="/img/culturales/'+arr[i]+'" alt="Evento Cultural Concepto3" data-caption="Evento">');
 }
